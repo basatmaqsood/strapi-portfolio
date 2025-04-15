acc5c1c28d9facce8c7c44e43d49a0ba03100c52
basat <itzbasatmaqsood@gmail.com>
2025-04-16 01:25:02 +0500

    TYPES ADDED

--- config/server.js
+++ config/server.js
@@ -2,8 +2,14 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://strapi.basatmaqsood.com'), // Add this line for the public URL
-  admin: {
-    proxy: true,  // Enable proxy
-    url: '/admin', // Specify the admin panel URL path
+  app: {
+    keys: env.array('APP_KEYS'),
   },
+  cron: {
+    enabled: true,
+  },
+  webhooks: {
+    enabled: true,
+  },
+  proxy: true,
 });
