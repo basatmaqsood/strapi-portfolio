2cc3a8f99315313ad162cd05539f435ecbd7cb4f
basat <itzbasatmaqsood@gmail.com>
2025-04-16 01:44:11 +0500

    TYPES ADDED

--- src/admin/vite.config.js
+++ src/admin/vite.config.js
@@ -1,10 +1,10 @@
-module.exports = {
-  server: {
-    host: '0.0.0.0', // You can also specify your host (e.g., 'strapi.basatmaqsood.com')
-    port: 3000, // or whichever port you're using
-    allowedHosts: [
-      'strapi.basatmaqsood.com', // Allow your domain here
-    ],
-    https: true, // Enable HTTPS for secure communication
-  },
+import { mergeConfig } from 'vite';
+
+export default (config) => {
+  // Important: always return the modified config
+  return mergeConfig(config, {
+    server: {
+        allowedHosts: true,
+    },
+  });
 };
