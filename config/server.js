9e36b51743da04a1b9b7944f62dcb9a84684500f
basat <itzbasatmaqsood@gmail.com>
2025-04-16 01:08:00 +0500

    TYPES ADDED

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8000),
+  url: 'https://strapi.basatmaqsood.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
